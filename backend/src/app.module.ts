4d12d3f0922a75ccb21bcc0912783bfa7d1645ea
Airlangga Pradana <ranggavfxartwork@gmail.com>
2024-10-11 13:52:59 +0700

    5th test deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ChatModule } from './chat/chat.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UserModule, ChatModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
