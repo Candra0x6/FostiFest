d3feb47052bbfe0738e4de9977675da682af5a58
Candra0x6 <kurniawancandram@gmail.com>
2024-10-12 12:25:30 +0700

    fix: correct import path for PrismaService in login.module.ts

--- backend/src/login/login.module.ts
+++ backend/src/login/login.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   providers: [PrismaService, LoginService],
